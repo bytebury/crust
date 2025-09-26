0d4f180affa3eba18c35ed8605651e2885ce8a47
Marcello <hey@marcello.gg>
2025-09-25 22:00:06 -0400

    feat: add default implementation of cannot

--- src/util/rbac.rs
+++ src/util/rbac.rs
@@ -2,4 +2,7 @@ use crate::domain::rbac::Action;
 
 pub trait Can<T> {
     fn can(&self, action: Action, resource: &T) -> bool;
+    fn cannot(&self, action: Action, resource: &T) -> bool {
+        !self.can(action, resource)
+    }
 }
